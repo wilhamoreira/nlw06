434f3ea8e505b18c5e5ff4553af2e58e14363e89
wilhamoreira <wilhadev@gmail.com>
2023-08-24 22:54:24 -0300

    Finalizando ajustes da modal.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,22 +2,41 @@ import  Modal  from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //Pegar todos os botões que existem com a classe chack
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
     //adicionar a escuta
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
+
 
 //Quando o botão delete for clicado ele abre a modal
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button =>{
-    button.addEventListener("click", evente =>{
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
 
+function handleClick(event, check = true){
+    event.preventDefault()
+    const text = check ? "marcar como lida " : "excluir "
+    const slug = check ? "check" : "delete"
+    const roomId = document.querySelector("#room-id").dataset.id
+    const questionID = event.target.dataset.id
+
+    const form = document.querySelector('.modal form')
+    form.setAttribute("action", `/room/${roomId}/${questionID}/${slug}`)
+
+    modalTitle.innerHTML = `${text}  esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text}`
+    check? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    //abrir moDAL
+    modal.open()
+}
--- src/route.js
+++ src/route.js
@@ -1,9 +1,11 @@
 const express = require('express')
-
+const QuestionController = require('./controllers/QuestionController')
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
+route.get('/', (req, res) => res.render("index", {page : 'enter-room'}))
 route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/create-pass', (req, res) => res.render("index", {page : 'create-pass'}))
+
+route.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = route
